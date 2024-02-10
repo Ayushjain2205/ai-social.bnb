fb29e100af663be8b3069bd39285881873a17d26
Ayush Jain <ayushjain2205@gmail.com>
2024-02-10 17:17:49 +0530

    Make top bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import InfiniteCanvas from "../components/InfiniteCanvas";
 import InfiniteCanvas2 from "../components/InfiniteCanvas2";
+import Page from "../components/Layout/Page";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
+    <Page>
       <InfiniteCanvas2 />
-    </div>
+    </Page>
   );
 }
