e62501ecfcced1067a96f086626c254d7f2e1920
Ayush Jain <ayushjain2205@gmail.com>
2024-02-13 01:40:38 +0530

    Config hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider>
+      <Toaster />
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
