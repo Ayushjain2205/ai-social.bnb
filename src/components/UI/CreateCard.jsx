05afe5a95e8555252cef2ad5811a8a8681b422da
Ayush Jain <ayushjain2205@gmail.com>
2024-02-16 14:32:07 +0530

    Link post to remix and feed

--- src/components/UI/CreateCard.jsx
+++ src/components/UI/CreateCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 const CreateCard = ({ name, description, imageUrl }) => {
   return (
-    <div className="relative flex flex-shrink-0 flex-col items-center h-[424px] w-[240px] bg-[#fff] rounded-[8px]">
-      <img className="p-[26px]" src={imageUrl} alt={name} />
-      <div className="flex flex-col p-[12px] absolute bottom-[18px]">
-        <p className="text-[#FF5705] text-[20px] font-[500]">{name}</p>
-        <p className="text-[#FF5705] text-[12px] leading-[18px]">
-          {description}
-        </p>
+    <Link href="/generate">
+      <div className="relative flex flex-shrink-0 flex-col items-center h-[424px] w-[240px] bg-[#fff] rounded-[8px]">
+        <img className="p-[26px]" src={imageUrl} alt={name} />
+        <div className="flex flex-col p-[12px] absolute bottom-[18px]">
+          <p className="text-[#FF5705] text-[20px] font-[500]">{name}</p>
+          <p className="text-[#FF5705] text-[12px] leading-[18px]">
+            {description}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/pages/remix.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Page from "../components/Layout/Page";
+
+const remix = () => {
+  return <Page>remix</Page>;
+};
+
+export default remix;
