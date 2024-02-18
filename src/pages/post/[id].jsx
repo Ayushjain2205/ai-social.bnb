941d777b9d7710cd46e700679ea654a2bbc3ee16
Ayush Jain <ayushjain2205@gmail.com>
2024-02-18 16:13:31 +0530

    back incase of no post

--- src/pages/post/[id].jsx
+++ src/pages/post/[id].jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import Page from "../../components/Layout/Page";
 import Post from "../../components/UI/Post";
-import { postData } from "../../helpers/postdata"; // Adjust the import path to where your postData is defined
+import { postData } from "../../helpers/postdata";
 
 const PostPage = () => {
   const router = useRouter();
@@ -13,7 +13,13 @@ const PostPage = () => {
   const postToDisplay = postData.find((post) => post.id === parseInt(id, 10));
 
   if (!postToDisplay) {
-    return <Page>Post not found</Page>;
+    return (
+      <Page back="/">
+        <div className="flex flex-col items-center justify-center h-[300px]">
+          Post not found
+        </div>
+      </Page>
+    );
   }
 
   return (
