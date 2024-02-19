809e7696f41b81e089a28e61793c19fecd5a68bb
Ayush Jain <ayushjain2205@gmail.com>
2024-02-19 13:01:29 +0530

    Get dynamic images from post data

--- src/components/UI/DynamicImage.jsx
+++ src/components/UI/DynamicImage.jsx
@@ -1,24 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { postData } from "../../helpers/postdata";
 
-const images = [
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708289732/feed/explore/v7ufe6tlcgxu2ix2rqe9.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708285179/feed/explore/zlwmelrfwyddwmapzl9i.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708289732/feed/explore/c40dcl3hsrxlrjuqfufq.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708285177/feed/explore/dlymtsyggboeqrtp57uv.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708289732/feed/explore/sdhcfmlbu6npm83tjs7v.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708285177/feed/explore/cdv5qb0zzof0qfjy5ysw.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708285176/feed/explore/okopi91todzwmczern2o.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708285178/feed/explore/gwkultyyp4ueuppxqtny.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708289723/feed/explore/cgkm7havxmyyx7cajtzo.png",
-  "https://res.cloudinary.com/dwwcarx9g/image/upload/v1708285176/feed/explore/d77krrethkx11tl2vynj.png",
-];
+// Filter postData for entries with remixid="weird-food" and map to their src attributes
+const weirdFoodImages = postData
+  .filter((post) => post.remixid === "weird-food")
+  .map((post) => post.src);
 
 const DynamicImage = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % weirdFoodImages.length);
     }, 3000); // Change the image every 3 seconds
 
     return () => clearInterval(intervalId); // Clear the interval on component unmount
@@ -28,7 +21,7 @@ const DynamicImage = () => {
     <div>
       <img
         className="rounded-[8px] fade-in"
-        src={images[currentIndex]}
+        src={weirdFoodImages[currentIndex]}
         alt=""
         key={currentIndex}
       />
